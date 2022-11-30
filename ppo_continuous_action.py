fd2d88e4dab44dd7e463594aad200afa3ca05d76
Best-Eminem <cyk_cd@163.com>
2022-11-30 22:45:30 +0800

    update:update ppo
    Signed-off-by: Best-Eminem <cyk_cd@163.com>

--- ppo_continuous_action.py
+++ ppo_continuous_action.py
@@ -13,9 +13,11 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.distributions.normal import Normal
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 from torch.utils.tensorboard import SummaryWriter
 from dataLoad.loadNii import get_spinedata
 from env import SingleSpineEnv
+from utils.img_utils import images_to_video
 
 def parse_args():
     # fmt: off
@@ -38,39 +40,39 @@ def parse_args():
         help="whether to capture videos of the agent performances (check out `videos` folder)")
 
     # Algorithm specific arguments
-    parser.add_argument("--env-id", type=str, default="HalfCheetahBulletEnv-v0",
+    parser.add_argument("--env_id", type=str, default="HalfCheetahBulletEnv-v0",
         help="the id of the environment")
-    parser.add_argument("--total-timesteps", type=int, default=5000,
+    parser.add_argument("--total_timesteps", type=int, default=20000,
         help="total timesteps of the experiments")
-    parser.add_argument("--learning-rate", type=float, default=3e-4,
+    parser.add_argument("--learning_rate", type=float, default=3e-4,
         help="the learning rate of the optimizer")
-    parser.add_argument("--num-envs", type=int, default=1,
+    parser.add_argument("--num_envs", type=int, default=1,
         help="the number of parallel game environments")
-    parser.add_argument("--num-steps", type=int, default=50,
+    parser.add_argument("--num_steps", type=int, default=100,
         help="the number of steps to run in each environment per policy rollout")
-    parser.add_argument("--anneal-lr", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+    parser.add_argument("--anneal_lr", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="Toggle learning rate annealing for policy and value networks")
     parser.add_argument("--gamma", type=float, default=0.99,
         help="the discount factor gamma")
-    parser.add_argument("--gae-lambda", type=float, default=0.95,
+    parser.add_argument("--gae_lambda", type=float, default=0.95,
         help="the lambda for the general advantage estimation")
-    parser.add_argument("--num-minibatches", type=int, default=5,
+    parser.add_argument("--num_minibatches", type=int, default=5,
         help="the number of mini-batches")
-    parser.add_argument("--update-epochs", type=int, default=10,
+    parser.add_argument("--update_epochs", type=int, default=10,
         help="the K epochs to update the policy")
-    parser.add_argument("--norm-adv", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+    parser.add_argument("--norm_adv", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="Toggles advantages normalization")
-    parser.add_argument("--clip-coef", type=float, default=0.2,
+    parser.add_argument("--clip_coef", type=float, default=0.2,
         help="the surrogate clipping coefficient")
-    parser.add_argument("--clip-vloss", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+    parser.add_argument("--clip_vloss", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="Toggles whether or not to use a clipped loss for the value function, as per the paper.")
-    parser.add_argument("--ent-coef", type=float, default=0.0,
+    parser.add_argument("--ent_coef", type=float, default=0.0,
         help="coefficient of the entropy")
-    parser.add_argument("--vf-coef", type=float, default=0.5,
+    parser.add_argument("--vf_coef", type=float, default=0.5,
         help="coefficient of the value function")
-    parser.add_argument("--max-grad-norm", type=float, default=0.5,
+    parser.add_argument("--max_grad_norm", type=float, default=0.5,
         help="the maximum norm for the gradient clipping")
-    parser.add_argument("--target-kl", type=float, default=None,
+    parser.add_argument("--target_kl", type=float, default=None,
         help="the target KL divergence threshold")
     args = parser.parse_args()
     args.batch_size = int(args.num_envs * args.num_steps)
@@ -99,7 +101,7 @@ def make_env(env_id, seed, idx, capture_video, run_name, gamma):
     return thunk
 
 def build_data_load(dataDir, pedicle_points, pedicle_points_in_zyx, input_z=64, input_y=80, input_x=160):
-    spine_data, spacing = get_spinedata(dataDir, pedicle_points,pedicle_points_in_zyx, input_z, input_y, input_x) #160，80，64 xyz
+    spine_data = get_spinedata(dataDir, pedicle_points,pedicle_points_in_zyx, input_z, input_y, input_x) #160，80，64 xyz
     # cfg.Env.step.line_rd = float(max(cfg.Env.step.line_rd, spacing)) # 根据spacing修改计算直线长度时的直线半径。
     return spine_data # namedtuple:
 
@@ -113,49 +115,58 @@ def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
     torch.nn.init.constant_(layer.bias, bias_const)
     return layer
 
+def evaluate(envs, agent, epoch):
+    next_obs_2d, next_obs = envs.reset(random_reset = False)
+    next_obs = torch.Tensor(next_obs).to(device)
+    next_done = torch.zeros(args.num_envs).to(device)
+    step = 0
+    fig = plt.figure()
+    rewards = 0
+    while step<100:
+        step +=1
+        # ALGO LOGIC: action logic
+        with torch.no_grad():
+            action, logprob, _, value = agent.get_action_and_value(next_obs.unsqueeze_(0).unsqueeze_(0) if len(next_obs.shape) == 3 else next_obs)
+        # TRY NOT TO MODIFY: execute the game and log data.
+        _, reward, done, others, next_obs = envs.step(action.squeeze_(0).cpu().numpy() if len(action.shape) == 2 else action)
+        next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor([1.] if done else [0.]).to(device)
+        rewards = rewards + reward
+
+        info = {'reward': rewards, 'r': reward, 'len_delta': others['len_delta'], 'radiu_delta': others['radius_delta'],
+                'epoch': 0, 'frame': step, 'action':'{:.3f}, {:.3f}'.format(action[0], action[1])}
+
+        fig = envs.render_(fig, info, is_vis=False, is_save_gif=True, img_save_path='./ppo_imgs')
+
+    images_to_video('./ppo_imgs', '*.jpg', isDelete=True, savename = 'Update%d'%(epoch))
 
 class Agent(nn.Module):
     def __init__(self):
         super().__init__()
-        self.critic = nn.Sequential(
+        self.network = nn.Sequential(
             layer_init(nn.Conv3d(1, 32, 8, stride=4)),
-            nn.Tanh(),
+            nn.ReLU(True),
             layer_init(nn.Conv3d(32, 64, 4, stride=2)),
-            nn.Tanh(),
+            nn.ReLU(True),
             layer_init(nn.Conv3d(64, 64, 3, stride=1)),
-            nn.Tanh(),
+            nn.ReLU(True),
             nn.Flatten(),
             layer_init(nn.Linear(64 * 16 * 6 * 4, 512)),
-            nn.Tanh(),
-            layer_init(nn.Linear(512, 64)),
-            nn.Tanh(),
-            layer_init(nn.Linear(64, 64)),
-            nn.Tanh(),
-            layer_init(nn.Linear(64, 1), std=1.0),
+            nn.ReLU(True),
+        )
+        self.critic = nn.Sequential(
+            layer_init(nn.Linear(512, 1), std=1),
         )
         self.actor_mean = nn.Sequential(
-            layer_init(nn.Conv3d(1, 32, 8, stride=4)),
-            nn.Tanh(),
-            layer_init(nn.Conv3d(32, 64, 4, stride=2)),
-            nn.Tanh(),
-            layer_init(nn.Conv3d(64, 64, 3, stride=1)),
-            nn.Tanh(),
-            nn.Flatten(),
-            layer_init(nn.Linear(64 * 16 * 6 * 4, 512)),
-            nn.Tanh(),
-            layer_init(nn.Linear(512, 64)),
-            nn.Tanh(),
-            layer_init(nn.Linear(64, 64)),
-            nn.Tanh(),
-            layer_init(nn.Linear(64, 2), std=0.01),
+            layer_init(nn.Linear(512, 2), std=0.01),
         )
-        self.actor_logstd = nn.Parameter(torch.zeros(1, 2))
+        self.actor_logstd = nn.Parameter(-0.5 * torch.ones(1, 2))
 
     def get_value(self, x):
-        return self.critic(x)
+        return self.critic(self.network(x / 2.0))
 
     def get_action_and_value(self, x, action=None):
-        action_mean = self.actor_mean(x)
+        hidden = self.network(x / 2.0)
+        action_mean = self.actor_mean(hidden)
         action_logstd = self.actor_logstd.expand_as(action_mean)
         action_std = torch.exp(action_logstd)
         probs = Normal(action_mean, action_std)
@@ -163,17 +174,70 @@ class Agent(nn.Module):
             action = probs.sample()
         # log_prob(value)计算value在定义的正态分布（mean,1）中对应的概率的对数
         action = torch.clamp(action, min=-1.0, max=1.0)
-        return action, probs.log_prob(action).sum(1), probs.entropy().sum(1), self.critic(x)
+        return action, probs.log_prob(action).sum(1), probs.entropy().sum(1), self.critic(hidden)
 
-
-if __name__ == "__main__":
-    # env parameters
-    cfg = {'deg_threshold':[-180., 180., -180., 180.],
+def evluateothers():
+    cfg = {'deg_threshold':[-360., 360., -360., 360.],
            'reset':{'rdrange':[-90, 90],
                     'state_shape':(160, 80, 64)},
            'step':{'rotate_mag':[5, 5]},}
     
     args = parse_args()
+    if not os.path.exists('./ppo_imgs'):
+        os.makedirs('./ppo_imgs')
+    if not os.path.exists('./ppo_snapshot'):
+        os.makedirs('./ppo_snapshot')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    dataDir = r'./spineData/sub-verse835_dir-iso_L2_seg-vert_msk.nii.gz'
+    pedicle_points = np.asarray([[31,57,63],[30,58,106]])
+    pedicle_points_in_zyx = True #坐标是zyx形式吗？
+    spine_data = build_data_load(dataDir, pedicle_points, pedicle_points_in_zyx, input_z=64, input_y=80, input_x=160) #spine_data 是一个包含了mask以及mask坐标矩阵以及椎弓根特征点的字典
+    '''---2 Build Environment  ---'''
+    envs = build_Env(spine_data, cfg['deg_threshold'], cfg)  # 只修改了初始化函数，其他函数待修改
+    # assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
+    agent = Agent()
+    Train_RESUME = 'ppo_snapshot_not_reset/ppo_160.pth' ## whether to resume training, set value to 'None' or the path to the previous model.
+    if Train_RESUME:
+        ckpt = torch.load(Train_RESUME)
+        # epoch = ckpt['epoch']
+        agent.load_state_dict(ckpt)
+    agent.to(device)
+    
+    next_obs_2d, next_obs = envs.reset(random_reset = False)
+    next_obs = torch.Tensor(next_obs).to(device)
+    next_done = torch.zeros(args.num_envs).to(device)
+    step = 0
+    fig = plt.figure()
+    rewards = 0
+    while step<200:
+        step +=1
+        # ALGO LOGIC: action logic
+        with torch.no_grad():
+            action, logprob, _, value = agent.get_action_and_value(next_obs.unsqueeze_(0).unsqueeze_(0) if len(next_obs.shape) == 3 else next_obs)
+        # TRY NOT TO MODIFY: execute the game and log data.
+        _, reward, done, others, next_obs = envs.step(action.squeeze_(0).cpu().numpy() if len(action.shape) == 2 else action)
+        next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor([1.] if done else [0.]).to(device)
+        rewards = rewards + reward
+
+        info = {'reward': rewards, 'r': reward, 'len_delta': others['len_delta'], 'radiu_delta': others['radius_delta'],
+                'epoch': 0, 'frame': step, 'action':'{:.3f}, {:.3f}'.format(action[0], action[1])}
+
+        fig = envs.render_(fig, info, is_vis=False, is_save_gif=True, img_save_path='./ppo_imgs')
+
+    images_to_video('./ppo_imgs', '*.jpg', isDelete=True, savename = 'Update%d'%(0))
+
+def train():
+    # env parameters
+    cfg = {'deg_threshold':[-360., 360., -360., 360.],
+           'reset':{'rdrange':[-90, 90],
+                    'state_shape':(160, 80, 64)},
+           'step':{'rotate_mag':[5, 5]},}
+    
+    args = parse_args()
+    if not os.path.exists('./ppo_imgs'):
+        os.makedirs('./ppo_imgs')
+    if not os.path.exists('./ppo_snapshot'):
+        os.makedirs('./ppo_snapshot')
     run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
     if args.track:
         import wandb
@@ -212,7 +276,6 @@ if __name__ == "__main__":
     '''---2 Build Environment  ---'''
     envs = build_Env(spine_data, cfg['deg_threshold'], cfg)  # 只修改了初始化函数，其他函数待修改
     # assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
-
     agent = Agent().to(device)
     optimizer = optim.Adam(agent.parameters(), lr=args.learning_rate, eps=1e-5)
 
@@ -227,12 +290,16 @@ if __name__ == "__main__":
     # TRY NOT TO MODIFY: start the game
     global_step = 0
     start_time = time.time()
-    next_obs_2d, next_obs = envs.reset()
+    next_obs_2d, next_obs = envs.reset(random_reset = False)
     next_obs = torch.Tensor(next_obs).to(device)
     next_done = torch.zeros(args.num_envs).to(device)
     num_updates = args.total_timesteps // args.batch_size
 
     for update in tqdm(range(1, num_updates + 1)):
+        # next_obs_2d, next_obs = envs.reset(random_reset = False)
+        # next_obs = torch.Tensor(next_obs).to(device)
+        # next_done = torch.zeros(args.num_envs).to(device)
+        
         # Annealing the rate if instructed to do so.
         if args.anneal_lr:
             frac = 1.0 - (update - 1.0) / num_updates
@@ -257,12 +324,10 @@ if __name__ == "__main__":
             rewards[step] = torch.tensor(reward).to(device).view(-1)
             next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor([1.] if done else [0.]).to(device)
 
-            # for item in info:
-            #     if "episode" in item.keys():
-            #         print(f"global_step={global_step}, episodic_return={item['episode']['r']}")
-            #         writer.add_scalar("charts/episodic_return", item["episode"]["r"], global_step)
-            #         writer.add_scalar("charts/episodic_length", item["episode"]["l"], global_step)
-            #         break
+            print(f"global_step={global_step}, episodic_return={reward}")
+            writer.add_scalar("charts/episodic_return", reward, global_step)
+            writer.add_scalar("charts/episodic_length", step+1, global_step)
+
 
         # bootstrap value if not done
         with torch.no_grad():
@@ -358,6 +423,13 @@ if __name__ == "__main__":
         writer.add_scalar("losses/explained_variance", explained_var, global_step)
         print("SPS:", int(global_step / (time.time() - start_time)))
         writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
-
+        if update%10 == 0:
+            torch.save(agent.state_dict(), './ppo_snapshot/'+'/ppo_%d.pth' % (update))
+            agent = agent.eval()
+            evaluate(envs, agent, update)
+            agent = agent.train()
     envs.close()
     writer.close()
+if __name__ == "__main__":
+    train()
+    # evluateothers()
